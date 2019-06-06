6097096255830113c79f4c4cc6c2a1ded575087c
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-06-05 20:25:33 -0700

    All stretch goals complete, added NavLinks with active class to saved movie list

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+
+import "../index.css";
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -12,7 +14,9 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
+          <NavLink to={`/movies/${movie.id}`} activeClassName="saved-active">
+            <span className="saved-movie">{movie.title}</span>
+          </NavLink>
         ))}
         <Link to="/">
           <div className="home-button">Home</div>
